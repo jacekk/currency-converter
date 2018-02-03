a6afe1306d2af5bed638470cba5eaa06b56b3d3f
jacekk <github.com@jacek-k.net>
2018-02-03 23:25:42 +0100

    moved App to components dir;

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,6 +7,10 @@ class App extends Component {
     render() {
         return (
             <div className="App">
+                {/*
+                <Controls />
+                <Results />
+                */}
                 <header className="App-header">
                     <img src={logo} className="App-logo" alt="logo" />
                     <h1 className="App-title">Welcome to React</h1>
