c816289ccfe3a4c2ca199fb5532fdc715e2387c1
jacekk <github.com@jacek-k.net>
2019-06-08 14:58:08 +0200

    v0.2.0 released;

---
+++ precache-manifest.fa236ed2121609764f3dc86694f7dc65.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b599d08a80a8d3fa2674e81b8f9014bf",
+    "url": "/currency-converter/index.html"
+  },
+  {
+    "revision": "e6f40ddaa65e5af675e3",
+    "url": "/currency-converter/static/css/2.582d1676.chunk.css"
+  },
+  {
+    "revision": "5d943c6bde890f9afa57",
+    "url": "/currency-converter/static/css/main.46de45dd.chunk.css"
+  },
+  {
+    "revision": "e6f40ddaa65e5af675e3",
+    "url": "/currency-converter/static/js/2.5329fd6b.chunk.js"
+  },
+  {
+    "revision": "5d943c6bde890f9afa57",
+    "url": "/currency-converter/static/js/main.5308b5e3.chunk.js"
+  },
+  {
+    "revision": "3fb806550804c3de195a",
+    "url": "/currency-converter/static/js/runtime~main.ac68a149.js"
+  }
+]);
